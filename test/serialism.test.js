a3a323937638084f774feb3527b47ef0b52045f7
tmhglnd <timohoogland@gmail.com>
2020-01-10 19:24:13 +0100

    small fixes

--- test/serialism.test.js
+++ test/serialism.test.js
@@ -2,25 +2,29 @@
 const Serialism = require("../index");
 const Gen = require("../index").Gen;
 
-test("Gen.spread(-5)");
-test("Gen.spread(5, 10)");
+/*
+	Test criteria:
+	- test for no argument
+	- test for single argument
+	- test for multiple arguments
+	- test for negative values
+*/
 
-test("Gen.spreadFloat(5, 0, 11)");
-test("Gen.spreadFloat(5, -4, 6)");
-test("Gen.spreadInclusiveFloat(5, -4, 4)");
+testGen();
 
-// test("Gen.spreadFloat(48, 0, 12)");
-// test("Gen.spreadExp(10, 10, 1000, 4)");
-// test("Gen.spreadinclusive(5, 0, 12)");
-// test("Gen.spread_f(5, 0, 1)");
-// test("Gen.spreadinclusive_f(6, 0, 1)");
-
-// test("Gen.spread(6, -100, 100)");
-// test("Gen.spreadinclusive(4, -50, -10)");
-// test("Gen.spread_f(5, 2)");
-// test("Gen.spreadinclusive_f(5, -2, 2)");
+function testGen(){
+	test("Gen.spread()");
+	test("Gen.spread(-5)");
+	test("Gen.spread(5, 10)");
+	test("Gen.spreadFloat(5, 0, 11)");
+	test("Gen.spreadFloat(4, -1, 1)");
+	test("Gen.spreadInclusiveFloat(5, -1, 1)");
+	test("Gen.spreadInclusiveFloatExp(5, 0, 1, 2)");
+}
 
 function test(f){
+	// print the written function to console
 	console.log(f);
+	// evaluate the function and print results
 	console.log("=> ", eval(f), "\n");
 }
