6385ebd13f86ab12340a7db0fa0198c04df9992a
James Bradbury <github@jamesbradbury.net>
2022-11-08 00:19:15 +0100

    update markov test

--- test/markov.test.js
+++ test/markov.test.js
@@ -13,10 +13,10 @@ let TS = require(entryPoint);
 fullTest(TS);
 
 // reload library with es5.min.js build
-// TS = require(es5build);
+TS = require(es5build);
 
 // and run full test with es5.min.js build;
-// fullTest(TS);
+fullTest(TS);
 
 function fullTest(Srl) {
 	const Rand = Srl.Stochastic;
